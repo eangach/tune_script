81a4d4de795e92b258c8d61400e561c297c20c09
Thomas Behrndt <thomas.behrndt@me.com>
2015-10-11 08:03:31 +1100

    Refactor osascipt into module TuneScript

--- lib/tune_script/osascript.rb
+++ lib/tune_script/osascript.rb
@@ -1,18 +1,20 @@
 require 'open3'
 
-# Run an AppleScript with osascript
-#
-# @param script [String] the script to run
-# @return [String] the output fom the script
-def osascript(script)
-  cmd = "osascript -e '#{script}'"
+module TuneScript
+  # Run an AppleScript with osascript
+  #
+  # @param script [String] the script to run
+  # @return [String] the output fom the script
+  def self.osascript(script)
+    cmd = "osascript -e '#{script}'"
 
-  o, e, s = Open3.capture3 cmd
+    o, e, s = Open3.capture3 cmd
 
-  unless s.success?
-    warn e
-    fail 'ERROR: osascript return code ' + s.exitstatus.to_s
+    unless s.success?
+      warn e
+      fail 'ERROR: osascript return code ' + s.exitstatus.to_s
+    end
+
+    o.chomp
   end
-
-  o.chomp
 end
--- spec/osascript_spec.rb
+++ spec/osascript_spec.rb
@@ -4,13 +4,13 @@ module TuneScript
   describe TuneScript do
     describe 'osascript' do
       it 'raises an error for bad scripts' do
-        exception = -> { osascript '.' }.must_raise RuntimeError
+        exception = -> { TuneScript.osascript '.' }.must_raise RuntimeError
         exception.message.must_equal 'ERROR: osascript return code 1'
       end
 
       it 'returns the script result' do
         script = %(set x to "some data"\nget x)
-        output = osascript script
+        output = TuneScript.osascript script
 
         output.must_equal 'some data'
       end
