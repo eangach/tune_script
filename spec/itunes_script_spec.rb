3431a5a6e5f76341feefe4d9389026267c2e72ff
Thomas Behrndt <thomas.behrndt@me.com>
2015-10-13 15:23:00 +1100

    Do not use iTunes in CI tests

--- spec/itunes_script_spec.rb
+++ spec/itunes_script_spec.rb
@@ -3,11 +3,14 @@ require_relative 'spec_helper'
 module TuneScript
   describe TuneScript do
     describe 'itunes_script' do
-      it 'runs a script with application "iTunes"' do
-        script = 'get player state'
-        output = TuneScript.itunes_script script
+      # Do not use iTunes in CI tests
+      unless ENV['CI'] == true.to_s
+        it 'runs a script with application "iTunes"' do
+          script = 'get player state'
+          output = TuneScript.itunes_script script
 
-        %w{playing paused stopped}.must_include output
+          %w{playing paused stopped}.must_include output
+        end
       end
     end
   end
